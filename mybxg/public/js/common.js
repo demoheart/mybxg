e3ff8d72de72b1b9fd0528d6923a6b03b0a0618d
zhangsan <2290975269@qq.com>
2017-08-30 08:37:40 +0800

    添加了require的应用

--- mybxg/public/js/common.js
+++ mybxg/public/js/common.js
@@ -1,23 +1,30 @@
+define(['jquery', 'cookie'], function ($) {
+    // NProgress.start();
+    // NProgress.done();
 
-	NProgress.start();
+    //  右侧菜单的伸缩切换
+    $('.navs ul').prev('a').on('click', function () {
+        $(this).next().slideToggle();
+    });
 
-	NProgress.done();
-
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
-
-	// 退出功能键
-	$('#logoutBtn').on('click',function () {
-		$.ajax({
-			type:'post',
-			dataType:'json',
-			url:'/api/logout',
-			success:function (data) {
-				if(data.code == 200){
-                    location.href = '/main/login'
-				}
+    // 退出功能键
+    $('#logoutBtn').on('click', function () {
+        $.ajax({
+            type: 'post',
+            dataType: 'json',
+            url: '/api/logout',
+            success: function (data) {
+                if (data.code == 200) {
+                    location.href = '/main/login';
+                }
             }
-		})
-		return false;
+        })
+        return false;
     })
+    var sessionId = $.cookie('PHPSESSID');
+    console.log(sessionId);
+    // 判断是否有   sessionID
+    if (!sessionId && location.pathname != '/main/login') {
+        location.href = '/main/login';
+    }
+});
---
+++ mybxg/public/js/login.js
@@ -0,0 +1,22 @@
+﻿define(['jquery'],function ($) {
+   // 给登陆按钮添加登录事件
+   $("#loginBtn").on('click',function () {
+       $.ajax({
+           type:'post',
+           url:'/api/login',
+           dataType:'json',
+           data:$('#loginForm').serialize(),
+           success:function (data) {
+               if(data.code == 200){
+                  location.href = '/main/index'
+               }else {
+                   alert('用户名或者密码输入错误');
+               }
+           },
+           error:function(){
+              alert('请输入用户名和密码');
+           }
+       })
+       return false;
+   })
+})
